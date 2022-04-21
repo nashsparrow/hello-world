f5701e98787d8a56f324a4d13af78c4f6d8806ca
Nadeesh Wannakuwaththa Waduge <nadeesh.wannakuwaththa-waduge@stud.th-deg.de>
2022-04-21 06:59:04 +0200

    enhancements

--- hello-world/pages/_app.tsx
+++ hello-world/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initializeApp } from "firebase/app";
+import configData  from "../config.json";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBdjjiex6DK-iO81k0tSqyr2T9VqkifEok",
-  authDomain: "react-hello-world-3c9e8.firebaseapp.com",
-  projectId: "react-hello-world-3c9e8",
-  storageBucket: "react-hello-world-3c9e8.appspot.com",
-  messagingSenderId: "203274028943",
-  appId: "1:203274028943:web:49ac9a774d456c2084edfa"
-};
+  apiKey: configData.firebaseConfig.apiKey,
+  authDomain: configData.firebaseConfig.authDomain,
+  projectId: configData.firebaseConfig.projectId,
+  storageBucket: configData.firebaseConfig.storageBucket,
+  messagingSenderId: configData.firebaseConfig.messagingSenderId,
+  appId: configData.firebaseConfig.appId
+}
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- hello-world/pages/index.tsx
+++ hello-world/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { InferGetStaticPropsType } from 'next'
+import { InferGetServerSidePropsType } from 'next'
 import * as React from 'react'
 
 export interface Message {
@@ -13,7 +13,7 @@ const BASE_URL: string = 'https://y6ac3cnvr2wn3mctarho3ap2hm0vlepi.lambda-url.us
 
 export default function IndexPage({
   messages,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [postList, setPostList] = React.useState(messages)
 
   return (
@@ -25,7 +25,7 @@ export default function IndexPage({
       }
 
 
-  export async function getStaticProps() {
+  export async function getServerSideProps() {
     console.log('its working')
     const res = await fetch(BASE_URL)
     const messages: Message = await res.json();
